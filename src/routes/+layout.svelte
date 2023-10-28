3772e5292fc2a981e6ab04f0e41c2e769c66d33e
Stuart Allan Rutherford <stalruth@gmail.com>
2023-10-28 21:19:30 +1100

    Add Favicon and credits

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,3 +7,14 @@ import '$lib/css/app.css';
 <main>
 <slot></slot>
 </main>
+<footer>
+  <details>
+    <summary>Credits</summary>
+    <dl>
+      <dt>Programming and Design</dt>
+      <dd><a href="https://stalruth.dev">Stuart Rutherford</a> (<a href="https://github.com/Stalruth/cut-explorer/">GitHub</a>)</dd>
+      <dt>Favicon</dt>
+      <dd><a href="https://github.com/twitter/twemoji">Twemoji by Twitter</a></dd>
+    </dl>
+  </details>
+</footer>
